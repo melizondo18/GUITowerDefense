47a19350d69c9d9613b65030e7c5245a2028e2fb
jennygong98 <114629733+jennygong98@users.noreply.github.com>
2023-04-25 23:51:08 -0600

    Arreglo
    Implementación del cronometro trabajando, reestructuración del programa, eliminar métodos, agregar puntajes al castillo

--- GUI TD/src/Juego.java
+++ GUI TD/src/Juego.java
@@ -1,6 +1,5 @@
 /* Movimientos del juego*/
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
@@ -14,7 +13,6 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
-import javax.swing.Timer;
 
 public abstract class Juego extends JFrame implements ActionListener {
 // MARCO DE LA APP
@@ -41,15 +39,12 @@ public abstract class Juego extends JFrame implements ActionListener {
     JLabel fondoJuego;
     ImageIcon imagenFondoJuego;
 
-    // CRONOMETRO
+    // TORRE
+    JLabel puntajeTorre1;
+    JLabel puntajeTorre2;
+    
+    // TIEMPO
     JLabel tiempo;
-    Thread hilo;
-    boolean cronometroActivo;
-    //Cronometro n = new Cronometro();
-    
-    // Para el label de la torre
-    private JLabel puntosValorLabel;
-    
    
     
     public Juego () {
@@ -182,21 +177,24 @@ public abstract class Juego extends JFrame implements ActionListener {
             public void mouseReleased(MouseEvent e) {
                 // Al dale click a iniciar debería aparecer el panel de menu
                 juego();
-                iniciarCronometro();
-                run();
             }
         });
     }
 
     public void juego() {
+        Torre torre1 = new Torre();
+        Torre torre2 = new Torre();
+        Cronometro crono = new Cronometro ();
+        
         panelMenu.setVisible(false);
 
-        // CREAMOS UN PANEL DEL TAMAÑO DE LA VENTNA
+                    // CREAMOS UN PANEL DEL TAMAÑO DE LA VENTNA
         panelJuego = new JPanel();
         panelJuego.setLayout(null);
         panelJuego.setBounds(0, 0, ventana.getWidth(), ventana.getHeight());
         panelJuego.setVisible(true);
-        //
+        
+                    // FONDE DEL JUEGO
         fondoJuego = new JLabel();
         fondoJuego.setBounds(0, 0, ventana.getWidth(), ventana.getHeight());
         imagenFondoJuego = new ImageIcon("Imagen/camino.png");
@@ -205,103 +203,26 @@ public abstract class Juego extends JFrame implements ActionListener {
         fondoJuego.setVisible(true);
         panelJuego.add(fondoJuego, 0);
 
-        // Panel de puntos Torre - Etiqueta "Puntos: "
-        JLabel puntosLabel = new JLabel("Puntos Torre: "+torre);
-        puntosLabel.setBounds(20,200,200,30);
-        puntosLabel.setFont(new Font("SANS_SERIF", Font.BOLD, 10));
-        panelJuego.add(puntosLabel, 0 );
-        //Etiqueta para mostrar la cantidad de puntos
-        puntosValorLabel = new JLabel("");
-        puntosValorLabel.setBounds(100,10,80,30);
-        puntosValorLabel.setFont(new Font("SANS_SERIF", Font.PLAIN, 20));
-        panelJuego.add(puntosValorLabel, 0);
-    
-//Etiqueta donde se colocara el tiempo 
-        tiempo = new JLabel("00:00:000");
-        tiempo.setBounds(500, 20, 200, 20);
-        tiempo.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20)); // tamaño de las letras y tipo de letra
-        tiempo.setHorizontalAlignment(JLabel.CENTER); // el numero este en el centro
-        tiempo.setForeground(Color.BLUE); //Los números se ponen azul
-        tiempo.setBackground(Color.WHITE);
-        tiempo.setOpaque(true); // permite que el setBackground se ponga blanco
-        tiempo.setVisible(true);
-        panelJuego.add(tiempo, 0);
+                    // PUNTOS DE LAS TORRES
+        // TORRE 1
+        puntajeTorre1 = new JLabel("Puntos Torre: " + torre1.getVida());
+        puntajeTorre1.setBounds(25,200,200,30);
+        puntajeTorre1.setFont(new Font("SANS_SERIF", Font.BOLD, 10));
+        panelJuego.add(puntajeTorre1, 0 );
+
+        // TORRE 2
+        puntajeTorre2 = new JLabel ("Puntos Torre: " + torre2.getVida());
+        puntajeTorre2.setBounds(1075,200,200,30);
+        puntajeTorre2.setFont(new Font("SANS_SERIF", Font.BOLD, 10));
+        panelJuego.add(puntajeTorre2, 0 );
+        
+                    //Etiqueta donde se colocara el tiempo 
+        panelJuego.add(crono.getTiempo(), 0);
         
 
         ventana.add(panelJuego);
         ventana.setVisible(true);
     }
-
-    // CRONOMETRO
-    public void run() {
-        Integer minutos = 0, segundos = 0, milesimas = 0;
-        //min es minutos, seg es segundos y mil es milesimas de segundo
-        String min = "", seg = "", mil = "";
-        try {
-            //Mientras cronometroActivo sea verdadero entonces seguira
-            //aumentando el tiempo
-            while (cronometroActivo) {
-                Thread.sleep(4);
-                //Incrementamos 4 milesimas de segundo
-                milesimas += 4;
-
-                //Cuando llega a 1000 osea 1 segundo aumenta 1 segundo
-                //y las milesimas de segundo de nuevo a 0
-                if (milesimas == 1000) {
-                    milesimas = 0;
-                    segundos += 1;
-                    //Si los segundos llegan a 60 entonces aumenta 1 los minutos
-                    //y los segundos vuelven a 0
-                    if (segundos == 60) {
-                        segundos = 0;
-                        minutos++;
-                    }
-                }
-
-                //Esto solamente es estetica para que siempre este en formato
-                //00:00:000
-                if (minutos < 10) {
-                    min = "0" + minutos;
-                } else {
-                    min = minutos.toString();
-                }
-                if (segundos < 10) {
-                    seg = "0" + segundos;
-                } else {
-                    seg = segundos.toString();
-                }
-
-                if (milesimas < 10) {
-                    mil = "00" + milesimas;
-                } else if (milesimas < 100) {
-                    mil = "0" + milesimas;
-                } else {
-                    mil = milesimas.toString();
-                }
-
-                //Colocamos en la etiqueta la informacion
-                tiempo.setText(min + ":" + seg + ":" + mil);
-            }
-        } catch (Exception e) {
-        }
-        //Cuando se reincie se coloca nuevamente en 00:00:000
-        tiempo.setText("00:00:000");
-    }
-    
-    public void iniciarCronometro() {
-        cronometroActivo = true;
-        hilo = new Thread( (Runnable) this);
-        hilo.start();
-    }
-
-    //Esto es para parar el cronometro
-    public void pararCronometro(){
-        cronometroActivo = false;
-    }
-    
-public void actualizarPuntos(int puntos) {
-    puntosValorLabel.setText(Integer.toString(puntos));
-}
 
 public static void main(String[] ar) {
         Juego formulario1 = new Juego() {
